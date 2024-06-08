0713ef3224ba47e0c20605282ecedc221ae73957
CE171612LocAn <anhvlce171612@fpt.edu.vn>
2024-06-08 17:18:41 +0700

    update footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,32 +1,42 @@
-import React from 'react'
-import "./../asset/css/Footer.css"
+import React from "react";
+import "./../asset/css/Footer.css";
+import qr from "./../asset/img/QR.png";
 function Footer() {
     return (
         <div className="footer">
-            <div>
-                <div>
-                    <h4>About Us</h4>
-                    <h5>Store addres</h5>
-                    <p>903, Nguyễn Văn Linh, Châu Phú, An Giang</p>
-                    <h5>Phone Number</h5>
-                    <p>0949-975-771</p>
-                    <h5>Business Hours</h5>
-                    <p>Mon_Sat 10am to 6pm</p>
-                    <p>Sun 11pm to 5pm</p>
+            <div className="first">
+                <div className="about">
+                    <h3>About Us</h3>
+                    <div>
+                        <h5>Store addres:</h5>
+                        <p>903, Nguyễn Văn Linh, Châu Phú, An Giang</p>
+                    </div>
+                    <div>
+                        <h5>Phone Number:</h5>
+                        <p>0949-975-771</p>
+                    </div>
+                    <div>
+                        <h5>Business Hours:</h5>
+                        <p>Mon_Sat 10am to 6pm</p>
+                        <p>Sun 11pm to 5pm</p>
+                    </div>
                 </div>
-                <div>
-                <h4>Help</h4>
-                <ul>
-                    <li>Search</li>
-                    <li>Contact us</li>
-                    <li>Sizing chart</li>
-                    <li>Join our </li>
-                </ul>
+                <div className="helpbig">
+                    <ul className="help">
+                        <h3>Help</h3>
+                        <li>Search</li>
+                        <li>Contact us</li>
+                        <li>Sizing chart</li>
+                        <li>Join our </li>
+                    </ul>
+                </div>
+                <div className="imga">
+                    <img src={qr} />
                 </div>
             </div>
-
+            <div className="second"></div>
         </div>
-    )
+    );
 }
 
-export default Footer
+export default Footer;
