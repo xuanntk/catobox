16952c0a0ad9d1065abb6556306b571be6df6694
CE171612LocAn <anhvlce171612@fpt.edu.vn>
2024-06-08 15:41:15 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Header from './sections/Header';
+import Header from './Sections/Header';
+import Footer from './Sections/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-
+import "./../asset/css/Footer.css"
 function Footer() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="footer">
+            <div>
+                <div>
+                    <h4>About Us</h4>
+                    <h5>Store addres</h5>
+                    <p>903, Nguyễn Văn Linh, Châu Phú, An Giang</p>
+                    <h5>Phone Number</h5>
+                    <p>0949-975-771</p>
+                    <h5>Business Hours</h5>
+                    <p>Mon_Sat 10am to 6pm</p>
+                    <p>Sun 11pm to 5pm</p>
+                </div>
+                <div>
+                <h4>Help</h4>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Footer
