358f6380cac0b91ab2feaf3cdcdadfe8f4d377b5
xuanntk <xuanntk2003@gmail.com>
2024-06-09 15:15:41 +0700

    add

--- src/component/Home/section3.tsx
+++ src/component/Home/section3.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import line from "./../../asset/img/line3.svg"
 
 export default function section3() {
   return (
     <div>
       <p>Bộ sưu tập theo chủ đề</p>
+      <img src={line} alt="" />
     </div>
   )
 }
