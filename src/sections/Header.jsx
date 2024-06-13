457a15632ad90f486ed51ac2799ecbd88e8884b1
xuanntk <xuanntk2003@gmail.com>
2024-06-13 21:19:00 +0700

    Co-authored-by: thientai1223 <thientai1223@users.noreply.github.com>

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -6,16 +6,17 @@ import menu from '../asset/img/menu.svg'
 
 function Header() {
   return (
-    <div className='header flex justify-between items-center pl-[112px] py-[44px] pr-[30px]'>
-      <img src={logo} />
-      <div className='flex gap-[70px]'>
+    <div className='header flex justify-between items-center 
+    pl-[112px] py-[44px] pr-[30px] '>
+      <img className="max-sm:w-[45vw]" src={logo} />
+      <div className='flex gap-[3vw] xl:gap-[70px] '>
         <ul className='menu'>
           <li>Trang chủ</li>
           <li>Sản phẩm</li>
           <li>Về catobox</li>
           <li>Liên hệ</li>
         </ul>
-        <div className='flex gap-9'>
+        <div className='flex gap-9 max-sm:gap-4'>
           <img src={shop} className='w-[37px] pt-2'/>
           <img src={find} className='w-[28px]'/>
           <img src={menu} className='w-[28px]'/>
