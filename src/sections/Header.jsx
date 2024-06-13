30b5b6c9a22b014a37393f04ac124194376da53b
xuanntk <xuanntk2003@gmail.com>
2024-06-13 23:40:20 +0700

    add navbar

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../asset/img/logo.svg'
 import shop from '../asset/img/shop.svg'
 import find from '../asset/img/find.svg'
@@ -6,8 +6,11 @@ import menu from '../asset/img/menu.svg'
 import { Link } from 'react-router-dom'
 
 function Header() {
+
+  const [isOpenMenu, setIsOpenMenu] = useState(true)
+
   return (
-    <div className='header flex justify-between items-center 
+    <div className='header max-sm:relative flex justify-between items-center 
     pl-[112px] py-[44px] pr-[30px] '>
       <img className="max-sm:w-[45vw]" src={logo} />
       <div className='flex gap-[3vw] xl:gap-[70px] '>
@@ -17,10 +20,19 @@ function Header() {
           <li><Link>Về catobox</Link></li>
           <li><Link>Liên hệ</Link></li>
         </ul>
-        <div className='flex gap-9 max-sm:gap-4'>
+        <div className='sm:relative flex gap-9 max-sm:gap-4'>
           <img src={shop} className='w-[37px] pt-2' />
           <img src={find} className='w-[28px]' />
-          <img src={menu} className='w-[28px]' />
+          <div className=' m-auto'>
+            <img src={menu} className='w-[28px]' onClick={()=>setIsOpenMenu(open => !open)}/>
+            {isOpenMenu && 
+            <ul className='menu-mobie'>
+              <li onClick={()=>setIsOpenMenu(open => !open)} ><Link to="/">Trang chủ</Link></li>
+              <li onClick={()=>setIsOpenMenu(open => !open)} ><Link to="product">Sản phẩm</Link></li>
+              <li onClick={()=>setIsOpenMenu(open => !open)} ><Link>Về catobox</Link></li>
+              <li onClick={()=>setIsOpenMenu(open => !open)} ><Link>Liên hệ</Link></li>
+            </ul>}
+          </div>
         </div>
       </div>
     </div>
