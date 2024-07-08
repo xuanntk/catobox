04a7c86ba4d7cc9c9f65876586d061ea8e4cb5af
CE171612LocAn <anhvlce171612@fpt.edu.vn>
2024-07-08 21:34:29 +0700

    an

--- src/component/product/Product.jsx
+++ src/component/product/Product.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import History from "./History"
+import product_1 from "./Product_Content1"
 export default function Product() {
   return (
     <div >
+      <product_1/>
       <History/>
     </div>
-  )
+  )``
 }
--- src/component/product/Product_Content1.jsx
+++ src/component/product/Product_Content1.jsx
@@ -5,6 +5,7 @@ function Product_Content1() {
     <div>
       <p className='text-[#96351E]'>Sản Phẩm > Nến Thơm</p>
       <p>Nến Thơm</p>
+      
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
     </div>
   )
