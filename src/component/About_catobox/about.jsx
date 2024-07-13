3f9e4aa4557c55063a3f1c7160c6d0fb8fe7174e
CE171612LocAn <anhvlce171612@fpt.edu.vn>
2024-07-13 23:45:01 +0700

    an

--- src/component/About_catobox/about.jsx
+++ src/component/About_catobox/about.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import About_content1 from "./About_content1"
+import About_content2 from "./About_content2"
+import About_content3  from './About_content3'
 function about() {
   return (
-    <div>
+    <div className="">
       <About_content1/>
+      <About_content2/>
+      <About_content3/>
     </div>
   )
 }
