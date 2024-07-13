0613b4ff45d7b9bfc4fdd89f7348c1a043c723cc
xuanntk <xuanntk2003@gmail.com>
2024-07-13 23:58:07 +0700

    fixfix

--- src/component/About_catobox/about.jsx
+++ src/component/About_catobox/about.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import About_content1 from "./About_content1"
 import About_content2 from "./About_content2"
 import About_content3  from './About_content3'
+import Header from '../../sections/Header'
+
 function about() {
   return (
     <div className="">
+      <Header/>
       <About_content1/>
       <About_content2/>
       <About_content3/>
